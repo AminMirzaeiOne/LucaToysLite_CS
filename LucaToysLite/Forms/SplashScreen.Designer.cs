6111ba996ee7522331b5ff22a0c617f862822c22
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-08 23:41:25 +0330

    - Add Radius Option To LoadingControl

--- LucaToysLite/Forms/SplashScreen.Designer.cs
+++ LucaToysLite/Forms/SplashScreen.Designer.cs
@@ -31,11 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.loadingControl1 = new LucaToysLite.Controls.LoadingControl();
+            this.button1 = new System.Windows.Forms.Button();
             this.lucaToysLogo1 = new LucaToysLite.Controls.LucaToysLogo();
             this.windowToolbar1 = new LucaToysLite.Controls.WindowToolbar();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -57,9 +56,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panel1.Controls.Add(this.loadingControl1);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.lucaToysLogo1);
             this.panel1.Controls.Add(this.windowToolbar1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -68,14 +66,27 @@
             this.panel1.Size = new System.Drawing.Size(894, 544);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // loadingControl1
             // 
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
-            this.label1.Location = new System.Drawing.Point(159, 344);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(560, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Paint += new System.Windows.Forms.PaintEventHandler(this.label1_Paint);
+            this.loadingControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.loadingControl1.Location = new System.Drawing.Point(145, 350);
+            this.loadingControl1.Name = "loadingControl1";
+            this.loadingControl1.Size = new System.Drawing.Size(560, 25);
+            this.loadingControl1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(24, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 62);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Version : 1.0.0";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // lucaToysLogo1
             // 
@@ -102,28 +113,6 @@
             this.windowToolbar1.TabIndex = 0;
             this.windowToolbar1.Window = this;
             // 
-            // label2
-            // 
-            this.label2.BackColor = System.Drawing.Color.RoyalBlue;
-            this.label2.Location = new System.Drawing.Point(159, 344);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 25);
-            this.label2.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
-            this.button1.FlatAppearance.BorderSize = 2;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(24, 456);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(206, 62);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Version : 1.0.0";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // SplashScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
@@ -148,8 +137,7 @@
         private System.Windows.Forms.Panel panel1;
         private Controls.WindowToolbar windowToolbar1;
         private Controls.LucaToysLogo lucaToysLogo1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private Controls.LoadingControl loadingControl1;
     }
 }
