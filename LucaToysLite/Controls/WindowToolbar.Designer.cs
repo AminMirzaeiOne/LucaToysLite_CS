589c158a6d1a5dcd0d3fbf7c28fd2291c0b71a4a
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-08 22:05:12 +0330

    - Resize WindowToolbar Control

--- LucaToysLite/Controls/WindowToolbar.Designer.cs
+++ LucaToysLite/Controls/WindowToolbar.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // WindowToolbar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "WindowToolbar";
+            this.Size = new System.Drawing.Size(559, 79);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
