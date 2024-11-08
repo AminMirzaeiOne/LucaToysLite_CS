4c55091f2b27f17fafa61e6efcabaf5a639c24b5
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-08 22:22:11 +0330

    - Change Startup Form In Program.cs

--- LucaToysLite/Program.cs
+++ LucaToysLite/Program.cs
@@ -16,7 +16,7 @@ namespace LucaToysLite
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LucaToysLite.Forms.SplashScreen());
         }
     }
 }
