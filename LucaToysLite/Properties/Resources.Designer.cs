92b4e6dfd0e8727dd0027e21f9be6aa212ac40ff
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-10 00:37:25 +0330

    - Add DStartupSound Audio File

--- LucaToysLite/Properties/Resources.Designer.cs
+++ LucaToysLite/Properties/Resources.Designer.cs
@@ -121,6 +121,16 @@ namespace LucaToysLite.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] DStartupSound {
+            get {
+                object obj = ResourceManager.GetObject("DStartupSound", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap LucaToys {
