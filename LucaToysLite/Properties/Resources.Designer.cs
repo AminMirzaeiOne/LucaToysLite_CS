3c7d947e002734fe15c11172f12047e9bb0dc667
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-10 00:35:17 +0330

    - Add BStartupSound File

--- LucaToysLite/Properties/Resources.Designer.cs
+++ LucaToysLite/Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace LucaToysLite.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] BStartupSound {
+            get {
+                object obj = ResourceManager.GetObject("BStartupSound", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap LucaToys {
