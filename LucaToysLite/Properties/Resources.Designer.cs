377ed12ee9814b2d1d0023e7edbf02333f9189cc
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-10 00:35:52 +0330

    - Add CSound Audio File

--- LucaToysLite/Properties/Resources.Designer.cs
+++ LucaToysLite/Properties/Resources.Designer.cs
@@ -91,6 +91,16 @@ namespace LucaToysLite.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] CSound {
+            get {
+                object obj = ResourceManager.GetObject("CSound", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap LucaToys {
