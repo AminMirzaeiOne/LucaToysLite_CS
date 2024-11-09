756d9187992e352a8129109107dbb794007198ea
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-10 00:39:07 +0330

    - Add HSound Audio File

--- LucaToysLite/Properties/Resources.Designer.cs
+++ LucaToysLite/Properties/Resources.Designer.cs
@@ -151,6 +151,16 @@ namespace LucaToysLite.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] HSound {
+            get {
+                object obj = ResourceManager.GetObject("HSound", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap LucaToys {
