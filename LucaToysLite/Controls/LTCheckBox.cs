c5a622bc64b9b43ffd62420b338db2e89c640cb4
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-11 16:12:40 +0330

    - Create Radius Property In LTCheckBox Control

--- LucaToysLite/Controls/LTCheckBox.cs
+++ LucaToysLite/Controls/LTCheckBox.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LucaToysLite.Controls
 {
-    internal class LTCheckBox
+    public class LTCheckBox : System.Windows.Forms.CheckBox
     {
+        private int radius = 20;
+        [DefaultValue(20)]
+        public int Radius
+        {
+            get { return radius; }
+            set
+            {
+                radius = value;
+                this.RecreateRegion();
+            }
+        }
+
+
     }
 }
