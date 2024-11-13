6e2b5b7df4ce17f7cf2a2c7ed7c8b475e414b0aa
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-13 13:47:51 +0330

    - UPdate LTMessageBox Control

--- LucaToysLite/Controls/LTMessageBox.cs
+++ LucaToysLite/Controls/LTMessageBox.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace LucaToysLite.Controls
 {
-    internal class LTMessageBox
+    public static class LTMessageBox
     {
+        public enum IconTypes { Error, Warning, Information }
+        public enum ActionTypes { Close, Restart, Exit }
+        public enum StartSounds { GSound, DSound, CSound }
+
+        public static void Show(string title, string caption)
+        {
+
+        }
+
+        public static void Show(string title, string caption, string description)
+        {
+
+        }
+
+        public static void Show(string title, string caption, string description, IconTypes icontype)
+        {
+
+        }
+
+        public static void Show(string title, string caption, string description, System.Drawing.Image icon)
+        {
+
+        }
+
+        public static void Show(string title, string caption, string description, IconTypes icontype, ActionTypes actiontype)
+        {
+
+        }
+
+        public static void Show(string title, string caption, string description, System.Drawing.Image icon, ActionTypes actiontype)
+        {
+
+        }
+        public static void Show(string title, string caption, string description, System.Drawing.Image icon, ActionTypes actiontype, StartSounds sound)
+        {
+
+        }
+        public static void Show(string title, string caption, string description, IconTypes icontype, ActionTypes actiontype, StartSounds sound)
+        {
+
+        }
     }
 }
