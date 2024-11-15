e155d59d93f831c3f9cf47a970c20adc90d09bba
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-15 03:48:20 +0330

    - Update License Page Ui

--- LucaToysLite/Pages/About/License.Designer.cs
+++ LucaToysLite/Pages/About/License.Designer.cs
@@ -45,16 +45,16 @@
             this.roundedPanel1.BorderSize = ((byte)(0));
             this.roundedPanel1.Controls.Add(this.label2);
             this.roundedPanel1.Controls.Add(this.label1);
-            this.roundedPanel1.Location = new System.Drawing.Point(25, 35);
+            this.roundedPanel1.Location = new System.Drawing.Point(25, 24);
             this.roundedPanel1.Name = "roundedPanel1";
-            this.roundedPanel1.Size = new System.Drawing.Size(696, 128);
+            this.roundedPanel1.Size = new System.Drawing.Size(696, 115);
             this.roundedPanel1.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(112, 26);
+            this.label2.Location = new System.Drawing.Point(112, 19);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(289, 79);
             this.label2.TabIndex = 0;
@@ -65,7 +65,7 @@
             // 
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Image = global::LucaToysLite.Properties.Resources.icons8_license_48;
-            this.label1.Location = new System.Drawing.Point(20, 25);
+            this.label1.Location = new System.Drawing.Point(20, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 79);
             this.label1.TabIndex = 0;
@@ -77,18 +77,21 @@
             this.roundedPanel2.BorderColor = System.Drawing.Color.Crimson;
             this.roundedPanel2.BorderSize = ((byte)(3));
             this.roundedPanel2.Controls.Add(this.label4);
-            this.roundedPanel2.Location = new System.Drawing.Point(25, 183);
+            this.roundedPanel2.Location = new System.Drawing.Point(25, 159);
             this.roundedPanel2.Name = "roundedPanel2";
-            this.roundedPanel2.Size = new System.Drawing.Size(696, 316);
+            this.roundedPanel2.Size = new System.Drawing.Size(696, 340);
             this.roundedPanel2.TabIndex = 1;
             // 
             // label4
             // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(20, 25);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(652, 268);
+            this.label4.Size = new System.Drawing.Size(652, 292);
             this.label4.TabIndex = 0;
             this.label4.Text = resources.GetString("label4.Text");
             // 
