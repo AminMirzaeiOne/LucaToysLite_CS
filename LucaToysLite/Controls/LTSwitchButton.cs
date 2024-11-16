6d934d47e58609eb4b784ff6096601b7f1efe23d
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-17 00:18:55 +0330

    - Create Based Fileds In LTSwitchButton Control

--- LucaToysLite/Controls/LTSwitchButton.cs
+++ LucaToysLite/Controls/LTSwitchButton.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LucaToysLite.Controls
 {
-    internal class LTSwitchButton
+    public class LTSwitchButton 
     {
+        private System.Drawing.Color onBackColor = Color.Crimson;
+        private System.Drawing.Color onToggleColor = Color.Linen;
+        private System.Drawing.Color offBackColor = Color.Gray;
+        private System.Drawing.Color offToggleColor = Color.Gainsboro;
+        private System.Boolean solidColor = true;
+
     }
 }
