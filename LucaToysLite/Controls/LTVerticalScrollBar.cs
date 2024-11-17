eb59da3032e1a8ff3986d62d47a463d42a6c2d0e
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-17 23:11:48 +0330

    - Create Radius Property In LTVerticalScrollBar Control

--- LucaToysLite/Controls/LTVerticalScrollBar.cs
+++ LucaToysLite/Controls/LTVerticalScrollBar.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LucaToysLite.Controls
 {
-    internal class LTVerticalScrollBar
+    public class LTVerticalScrollBar : System.Windows.Forms.VScrollBar
     {
+        private int radius = 20;
+        [DefaultValue(20)]
+        public int Radius
+        {
+            get { return radius; }
+            set
+            {
+                radius = value;
+                this.RecreateRegion();
+            }
+        }
+
     }
 }
