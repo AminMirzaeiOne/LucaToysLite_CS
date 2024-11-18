c96473a231bb3937475aa0d1de02b10e0658e258
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-18 22:44:18 +0330

    - Update Appearance Ui Page

--- LucaToysLite/Pages/Settings/Appearance.Designer.cs
+++ LucaToysLite/Pages/Settings/Appearance.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            WFAnimations.Animation animation2 = new WFAnimations.Animation();
+            WFAnimations.Animation animation1 = new WFAnimations.Animation();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Appearance));
             this.panel1 = new System.Windows.Forms.Panel();
             this.animator1 = new WFAnimations.Animator(this.components);
@@ -58,22 +58,22 @@
             // 
             this.animator1.AnimationType = WFAnimations.AnimationType.Leaf;
             this.animator1.Cursor = null;
-            animation2.AnimateOnlyDifferences = true;
-            animation2.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.BlindCoeff")));
-            animation2.LeafCoeff = 1F;
-            animation2.MaxTime = 1F;
-            animation2.MinTime = 0F;
-            animation2.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.MosaicCoeff")));
-            animation2.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation2.MosaicShift")));
-            animation2.MosaicSize = 0;
-            animation2.Padding = new System.Windows.Forms.Padding(0);
-            animation2.RotateCoeff = 0F;
-            animation2.RotateLimit = 0F;
-            animation2.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.ScaleCoeff")));
-            animation2.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.SlideCoeff")));
-            animation2.TimeCoeff = 0F;
-            animation2.TransparencyCoeff = 0F;
-            this.animator1.DefaultAnimation = animation2;
+            animation1.AnimateOnlyDifferences = true;
+            animation1.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.BlindCoeff")));
+            animation1.LeafCoeff = 1F;
+            animation1.MaxTime = 1F;
+            animation1.MinTime = 0F;
+            animation1.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicCoeff")));
+            animation1.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicShift")));
+            animation1.MosaicSize = 0;
+            animation1.Padding = new System.Windows.Forms.Padding(0);
+            animation1.RotateCoeff = 0F;
+            animation1.RotateLimit = 0F;
+            animation1.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.ScaleCoeff")));
+            animation1.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.SlideCoeff")));
+            animation1.TimeCoeff = 0F;
+            animation1.TransparencyCoeff = 0F;
+            this.animator1.DefaultAnimation = animation1;
             this.animator1.Upside = false;
             // 
             // roundedPanel5
@@ -192,6 +192,7 @@
             this.roundedRadioButton4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.roundedRadioButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.roundedRadioButton4.UseVisualStyleBackColor = false;
+            this.roundedRadioButton4.CheckedChanged += new System.EventHandler(this.roundedRadioButton4_CheckedChanged);
             // 
             // roundedRadioButton3
             // 
--- LucaToysLite/Pages/Settings/Appearance.cs
+++ LucaToysLite/Pages/Settings/Appearance.cs
@@ -17,22 +17,27 @@ namespace LucaToysLite.Pages.Settings
             InitializeComponent();
             this.panel1.Controls.Add(this.based);
             this.panel1.Controls.Add(this.fonts);
+            this.panel1.Controls.Add(this.region);
 
             this.based.Dock = DockStyle.Fill;
             this.fonts.Dock = DockStyle.Fill;
+            this.region.Dock = DockStyle.Fill;
 
             this.based.Visible = true;
             this.fonts.Visible = false;
+            this.region.Visible = false;
         }
 
         private LucaToysLite.Pages.Settings.AppearancePages.Based based = new AppearancePages.Based();
         private LucaToysLite.Pages.Settings.AppearancePages.Fonts fonts = new AppearancePages.Fonts();
+        private LucaToysLite.Pages.Settings.AppearancePages.Region region = new AppearancePages.Region();
 
         private void roundedRadioButton1_CheckedChanged(object sender, EventArgs e)
         {
             if (this.roundedRadioButton1.Checked)
             {
                 this.fonts.Visible = false;
+                this.region.Visible = false;
                 this.animator1.Show(this.based);
             }
         }
@@ -42,7 +47,18 @@ namespace LucaToysLite.Pages.Settings
             if (this.roundedRadioButton3.Checked)
             {
                 this.based.Visible = false;
+                this.region.Visible |= false;
                 this.animator1.Show(this.fonts);
+            }
+        }
+
+        private void roundedRadioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if(this.roundedRadioButton4.Checked)
+            {
+                this.fonts.Visible = false;
+                this.based.Visible = false;
+                this.animator1.Show(this.region);
             }
         }
     }
