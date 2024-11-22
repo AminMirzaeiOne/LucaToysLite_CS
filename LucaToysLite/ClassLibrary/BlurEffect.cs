38b60e35a57704a66c32e019dafd0a109c65597a
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-11-22 11:05:23 +0330

    - Create AccentState Enum In BlurEffect Class

--- LucaToysLite/ClassLibrary/BlurEffect.cs
+++ LucaToysLite/ClassLibrary/BlurEffect.cs
@@ -8,6 +8,14 @@ namespace LucaToysLite.ClassLibrary
 {
     public class BlurEffect
     {
+        public enum AccentState
+        {
+            ACCENT_DISABLED = 0,
+            ACCENT_ENABLE_GRADIENT = 1,
+            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+            ACCENT_ENABLE_BLURBEHIND = 3,
+            ACCENT_INVALID_STATE = 4
+        }
 
     }
 }
