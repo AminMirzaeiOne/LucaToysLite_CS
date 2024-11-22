c490fa57276210a815dabb3c3fa1d25753ec28de
Amin Mirzaei (امین میرزایی) <aminmirzaei.og@gmail.com>
2024-11-22 14:58:28 +0330

    - Update LTVertScrollBar Control Code

--- LucaToysLite/Controls/LTVertScrollBar.Designer.cs
+++ LucaToysLite/Controls/LTVertScrollBar.Designer.cs
@@ -28,19 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.roundedButton1 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedButton2 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedButton3 = new LucaToysLite.Controls.RoundedButton();
+            this.timerBottom = new System.Windows.Forms.Timer(this.components);
+            this.timerTop = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // roundedButton1
+            // 
+            this.roundedButton1.BackColor = System.Drawing.Color.Crimson;
+            this.roundedButton1.BorderRadius = ((byte)(10));
+            this.roundedButton1.BorderSize = ((byte)(3));
+            this.roundedButton1.EnableBorder = true;
+            this.roundedButton1.FlatAppearance.BorderSize = 0;
+            this.roundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 7F, System.Drawing.FontStyle.Bold);
+            this.roundedButton1.ForeColor = System.Drawing.Color.White;
+            this.roundedButton1.Location = new System.Drawing.Point(8, 6);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.Size = new System.Drawing.Size(25, 25);
+            this.roundedButton1.TabIndex = 0;
+            this.roundedButton1.Text = "";
+            this.roundedButton1.UseVisualStyleBackColor = false;
+            this.roundedButton1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.roundedButton1_MouseDown);
+            this.roundedButton1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.roundedButton1_MouseUp);
+            // 
+            // roundedButton2
+            // 
+            this.roundedButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.roundedButton2.BackColor = System.Drawing.Color.Crimson;
+            this.roundedButton2.BorderRadius = ((byte)(10));
+            this.roundedButton2.BorderSize = ((byte)(3));
+            this.roundedButton2.EnableBorder = true;
+            this.roundedButton2.FlatAppearance.BorderSize = 0;
+            this.roundedButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton2.Font = new System.Drawing.Font("Segoe MDL2 Assets", 7F, System.Drawing.FontStyle.Bold);
+            this.roundedButton2.ForeColor = System.Drawing.Color.White;
+            this.roundedButton2.Location = new System.Drawing.Point(8, 369);
+            this.roundedButton2.Name = "roundedButton2";
+            this.roundedButton2.Size = new System.Drawing.Size(25, 25);
+            this.roundedButton2.TabIndex = 0;
+            this.roundedButton2.Text = "";
+            this.roundedButton2.UseVisualStyleBackColor = false;
+            this.roundedButton2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.roundedButton2_MouseDown);
+            this.roundedButton2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.roundedButton2_MouseUp);
+            // 
+            // roundedButton3
+            // 
+            this.roundedButton3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.roundedButton3.BackColor = System.Drawing.Color.Crimson;
+            this.roundedButton3.BorderRadius = ((byte)(10));
+            this.roundedButton3.BorderSize = ((byte)(3));
+            this.roundedButton3.EnableBorder = true;
+            this.roundedButton3.FlatAppearance.BorderSize = 0;
+            this.roundedButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton3.Font = new System.Drawing.Font("Segoe MDL2 Assets", 8F, System.Drawing.FontStyle.Bold);
+            this.roundedButton3.ForeColor = System.Drawing.Color.White;
+            this.roundedButton3.Location = new System.Drawing.Point(8, 122);
+            this.roundedButton3.Name = "roundedButton3";
+            this.roundedButton3.Size = new System.Drawing.Size(25, 207);
+            this.roundedButton3.TabIndex = 0;
+            this.roundedButton3.UseVisualStyleBackColor = false;
+            this.roundedButton3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.roundedButton3_MouseDown);
+            this.roundedButton3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.roundedButton3_MouseMove);
+            this.roundedButton3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.roundedButton3_MouseUp);
+            // 
+            // timerBottom
+            // 
+            this.timerBottom.Interval = 10;
+            this.timerBottom.Tick += new System.EventHandler(this.timerBottom_Tick);
+            // 
+            // timerTop
+            // 
+            this.timerTop.Interval = 10;
+            this.timerTop.Tick += new System.EventHandler(this.timerTop_Tick);
             // 
             // LTVertScrollBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Controls.Add(this.roundedButton3);
+            this.Controls.Add(this.roundedButton2);
+            this.Controls.Add(this.roundedButton1);
             this.Name = "LTVertScrollBar";
             this.Size = new System.Drawing.Size(40, 400);
+            this.DoubleClick += new System.EventHandler(this.LTVertScrollBar_DoubleClick);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private RoundedButton roundedButton1;
+        private RoundedButton roundedButton2;
+        private RoundedButton roundedButton3;
+        private System.Windows.Forms.Timer timerBottom;
+        private System.Windows.Forms.Timer timerTop;
     }
 }
--- LucaToysLite/Controls/LTVertScrollBar.cs
+++ LucaToysLite/Controls/LTVertScrollBar.cs
@@ -18,91 +18,184 @@ namespace LucaToysLite.Controls
             InitializeComponent();
         }
 
-        public class RoundedPanel : System.Windows.Forms.Panel
+        private int radius = 20;
+        private bool mouseDown;
+        private Point lastLocation;
+
+        [DefaultValue(20)]
+        public int Radius
         {
-            private int radius = 20;
-            [DefaultValue(20)]
-            public int Radius
+            get { return radius; }
+            set
             {
-                get { return radius; }
-                set
-                {
-                    radius = value;
-                    this.RecreateRegion();
-                }
-            }
-
-            public System.Boolean EnableBorder { get; set; } = true;
-
-            public System.Drawing.Color BorderColor { get; set; } = Color.Crimson;
-            public System.Byte BorderSize { get; set; } = 2;
-
-            [System.Runtime.InteropServices.DllImport("gdi32.dll")]
-            private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect,
-                int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
-
-            private GraphicsPath GetRoundRectagle(Rectangle bounds, int radius)
-            {
-                float r = radius;
-                GraphicsPath path = new GraphicsPath();
-                path.StartFigure();
-                path.AddArc(bounds.Left, bounds.Top, r, r, 180, 90);
-                path.AddArc(bounds.Right - r, bounds.Top, r, r, 270, 90);
-                path.AddArc(bounds.Right - r, bounds.Bottom - r, r, r, 0, 90);
-                path.AddArc(bounds.Left, bounds.Bottom - r, r, r, 90, 90);
-                path.CloseFigure();
-                return path;
-            }
-
-            private void RecreateRegion()
-            {
-                var bounds = ClientRectangle;
-                this.Region = Region.FromHrgn(CreateRoundRectRgn(bounds.Left, bounds.Top,
-                    bounds.Right, bounds.Bottom, Radius, radius));
-                this.Invalidate();
-            }
-
-            protected override void OnSizeChanged(EventArgs e)
-            {
-                base.OnSizeChanged(e);
+                radius = value;
                 this.RecreateRegion();
             }
+        }
 
-            GraphicsPath GetRoundPath(RectangleF Rect, int radius)
+        public System.Byte Value { get; set; } = 0;
+        public System.Int32 ScrollHeight
+        {
+            get { return this.roundedButton3.Size.Height; }
+            set
             {
-                float r2 = radius / 2f;
-                GraphicsPath GraphPath = new GraphicsPath();
-
-                GraphPath.AddArc(Rect.X, Rect.Y, radius, radius, 180, 90);
-                GraphPath.AddLine(Rect.X + r2, Rect.Y, Rect.Width - r2, Rect.Y);
-                GraphPath.AddArc(Rect.X + Rect.Width - radius, Rect.Y, radius, radius, 270, 90);
-                GraphPath.AddLine(Rect.Width, Rect.Y + r2, Rect.Width, Rect.Height - r2);
-                GraphPath.AddArc(Rect.X + Rect.Width - radius,
-                                 Rect.Y + Rect.Height - radius, radius, radius, 0, 90);
-                GraphPath.AddLine(Rect.Width - r2, Rect.Height, Rect.X + r2, Rect.Height);
-                GraphPath.AddArc(Rect.X, Rect.Y + Rect.Height - radius, radius, radius, 90, 90);
-                GraphPath.AddLine(Rect.X, Rect.Height - r2, Rect.X, Rect.Y + r2);
-                GraphPath.CloseFigure();
-                return GraphPath;
+                this.roundedButton3.Height = value;
             }
+        }
 
-            protected override void OnPaint(PaintEventArgs e)
+        public System.Boolean EnableBorder { get; set; } = true;
+
+        public System.Drawing.Color BorderColor { get; set; } = Color.Crimson;
+        public System.Byte BorderSize { get; set; } = 2;
+
+        public System.Drawing.Color ColorPallet
+        {
+            get { return this.roundedButton1.BackColor; }
+            set
             {
-                base.OnPaint(e);
-                e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
-                RectangleF Rect = new RectangleF(0, 0, this.Width, this.Height);
-                using (GraphicsPath GraphPath = GetRoundPath(Rect, this.Radius))
+                this.roundedButton1.BackColor = value;
+                this.roundedButton2.BackColor = value;
+                this.roundedButton3.BackColor = value;
+                this.BorderColor = value;
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect,
+            int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        private GraphicsPath GetRoundRectagle(Rectangle bounds, int radius)
+        {
+            float r = radius;
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(bounds.Left, bounds.Top, r, r, 180, 90);
+            path.AddArc(bounds.Right - r, bounds.Top, r, r, 270, 90);
+            path.AddArc(bounds.Right - r, bounds.Bottom - r, r, r, 0, 90);
+            path.AddArc(bounds.Left, bounds.Bottom - r, r, r, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        private void RecreateRegion()
+        {
+            var bounds = ClientRectangle;
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(bounds.Left, bounds.Top,
+                bounds.Right, bounds.Bottom, Radius, radius));
+            this.Invalidate();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            this.RecreateRegion();
+        }
+
+        GraphicsPath GetRoundPath(RectangleF Rect, int radius)
+        {
+            float r2 = radius / 2f;
+            GraphicsPath GraphPath = new GraphicsPath();
+
+            GraphPath.AddArc(Rect.X, Rect.Y, radius, radius, 180, 90);
+            GraphPath.AddLine(Rect.X + r2, Rect.Y, Rect.Width - r2, Rect.Y);
+            GraphPath.AddArc(Rect.X + Rect.Width - radius, Rect.Y, radius, radius, 270, 90);
+            GraphPath.AddLine(Rect.Width, Rect.Y + r2, Rect.Width, Rect.Height - r2);
+            GraphPath.AddArc(Rect.X + Rect.Width - radius,
+                             Rect.Y + Rect.Height - radius, radius, radius, 0, 90);
+            GraphPath.AddLine(Rect.Width - r2, Rect.Height, Rect.X + r2, Rect.Height);
+            GraphPath.AddArc(Rect.X, Rect.Y + Rect.Height - radius, radius, radius, 90, 90);
+            GraphPath.AddLine(Rect.X, Rect.Height - r2, Rect.X, Rect.Y + r2);
+            GraphPath.CloseFigure();
+            return GraphPath;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+            RectangleF Rect = new RectangleF(0, 0, this.Width, this.Height);
+            using (GraphicsPath GraphPath = GetRoundPath(Rect, this.Radius))
+            {
+                this.Region = new Region(GraphPath);
+                using (Pen pen = new Pen(this.BorderColor, this.BorderSize))
                 {
-                    this.Region = new Region(GraphPath);
-                    using (Pen pen = new Pen(this.BorderColor, this.BorderSize))
-                    {
-                        pen.Alignment = PenAlignment.Inset;
-                        if (this.EnableBorder)
-                            e.Graphics.DrawPath(pen, GraphPath);
-                    }
+                    pen.Alignment = PenAlignment.Inset;
+                    if (this.EnableBorder)
+                        e.Graphics.DrawPath(pen, GraphPath);
                 }
             }
+        }
 
+        private void roundedButton3_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.mouseDown = true;
+            this.lastLocation = e.Location;
+        }
+
+        private void roundedButton3_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.mouseDown = false;
+        }
+
+        private void roundedButton3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.mouseDown == true)
+            {
+                if (this.roundedButton3.Top > this.roundedButton1.Bottom && this.roundedButton3.Bottom < this.roundedButton2.Top)
+                {
+                    this.roundedButton3.Location = new Point(
+                    this.roundedButton3.Location.X, (this.roundedButton3.Location.Y - this.lastLocation.Y) + e.Y);
+
+                    this.Update();
+                }
+                    
+            }
+        }
+
+        private void roundedButton2_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.timerBottom.Start();
+        }
+
+        private void timerBottom_Tick(object sender, EventArgs e)
+        {
+            if (this.roundedButton3.Bottom < this.roundedButton2.Top)
+            {
+                this.roundedButton3.Location = new Point(this.roundedButton3.Location.X, this.roundedButton3.Location.Y + 5);
+
+                this.Update();
+            }
+        }
+
+        private void roundedButton2_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.timerBottom.Stop();
+        }
+
+        private void timerTop_Tick(object sender, EventArgs e)
+        {
+            if (this.roundedButton3.Top > this.roundedButton1.Bottom)
+            {
+                this.roundedButton3.Location = new Point(this.roundedButton3.Location.X, this.roundedButton3.Location.Y - 5);
+
+                this.Update();
+            }
+        }
+
+        private void roundedButton1_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.timerTop.Start();
+        }
+
+        private void roundedButton1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.timerTop.Stop();
+        }
+
+        private void LTVertScrollBar_DoubleClick(object sender, EventArgs e)
+        {
+            this.roundedButton3.Top = Cursor.Clip.Location.Y;
         }
     }
+
 }
