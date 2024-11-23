b1eb743e1bef00c1737291cf3cf53562b937ed7f
Amin Mirzaei (امین میرزایی) <aminmirzaei.og@gmail.com>
2024-11-23 20:54:32 +0330

    - Update Search User Form Ui

--- LucaToysLite/Forms/SearchUser.Designer.cs
+++ LucaToysLite/Forms/SearchUser.Designer.cs
@@ -36,6 +36,10 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.roundedButton5 = new LucaToysLite.Controls.RoundedButton();
             this.windowToolbar1 = new LucaToysLite.Controls.WindowToolbar();
+            this.ltComboBox3 = new LucaToysLite.Controls.LTComboBox();
+            this.ltComboBox4 = new LucaToysLite.Controls.LTComboBox();
+            this.roundedPanel3 = new LucaToysLite.Controls.RoundedPanel();
+            this.roundedPanel5 = new LucaToysLite.Controls.RoundedPanel();
             this.roundedPanel2.SuspendLayout();
             this.roundedPanel1.SuspendLayout();
             this.roundedPanel4.SuspendLayout();
@@ -48,11 +52,13 @@
             this.roundedPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
             this.roundedPanel2.BorderColor = System.Drawing.Color.Crimson;
             this.roundedPanel2.BorderSize = ((byte)(3));
+            this.roundedPanel2.Controls.Add(this.ltComboBox4);
+            this.roundedPanel2.Controls.Add(this.ltComboBox3);
             this.roundedPanel2.Controls.Add(this.ltComboBox2);
             this.roundedPanel2.EnableBorder = true;
-            this.roundedPanel2.Location = new System.Drawing.Point(769, 62);
+            this.roundedPanel2.Location = new System.Drawing.Point(774, 62);
             this.roundedPanel2.Name = "roundedPanel2";
-            this.roundedPanel2.Size = new System.Drawing.Size(470, 70);
+            this.roundedPanel2.Size = new System.Drawing.Size(581, 70);
             this.roundedPanel2.TabIndex = 4;
             // 
             // ltComboBox2
@@ -102,7 +108,7 @@
             this.roundedPanel1.EnableBorder = true;
             this.roundedPanel1.Location = new System.Drawing.Point(25, 62);
             this.roundedPanel1.Name = "roundedPanel1";
-            this.roundedPanel1.Size = new System.Drawing.Size(727, 70);
+            this.roundedPanel1.Size = new System.Drawing.Size(738, 70);
             this.roundedPanel1.TabIndex = 4;
             // 
             // ltComboBox1
@@ -196,19 +202,115 @@
             this.windowToolbar1.MinimizeButton = true;
             this.windowToolbar1.MoveOption = true;
             this.windowToolbar1.Name = "windowToolbar1";
-            this.windowToolbar1.Size = new System.Drawing.Size(1260, 45);
+            this.windowToolbar1.Size = new System.Drawing.Size(1371, 45);
             this.windowToolbar1.StartupSound = LucaToysLite.Controls.WindowToolbar.StartupSounds.None;
             this.windowToolbar1.TabIndex = 0;
             this.windowToolbar1.TitleText = "Search User";
             this.windowToolbar1.WindowBorderSize = ((byte)(3));
+            // 
+            // ltComboBox3
+            // 
+            this.ltComboBox3.BorderRadius = ((byte)(20));
+            this.ltComboBox3.BorderSize = ((byte)(3));
+            this.ltComboBox3.ColorPallet = System.Drawing.Color.Crimson;
+            this.ltComboBox3.EnableBorder = true;
+            this.ltComboBox3.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.ltComboBox3.FlatAppearance.BorderSize = 0;
+            this.ltComboBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ltComboBox3.ForeColor = System.Drawing.Color.White;
+            this.ltComboBox3.Items = new string[] {
+        "Name",
+        "Family",
+        "Age",
+        "Usercode",
+        "Phone",
+        "Address",
+        "Account Number 1",
+        "Account Number 2"};
+            this.ltComboBox3.ItemsBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ltComboBox3.ItemsCheckedColor = System.Drawing.Color.Crimson;
+            this.ltComboBox3.ItemsFont = new System.Drawing.Font("Segoe UI Semibold", 9F);
+            this.ltComboBox3.ItemsForeColor = System.Drawing.Color.White;
+            this.ltComboBox3.ItemsHeight = ((byte)(40));
+            this.ltComboBox3.Location = new System.Drawing.Point(203, 11);
+            this.ltComboBox3.Name = "ltComboBox3";
+            this.ltComboBox3.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.ltComboBox3.Size = new System.Drawing.Size(175, 45);
+            this.ltComboBox3.TabIndex = 1;
+            this.ltComboBox3.Text = "Search Based";
+            this.ltComboBox3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ltComboBox3.TopDirection = false;
+            this.ltComboBox3.UseVisualStyleBackColor = true;
+            // 
+            // ltComboBox4
+            // 
+            this.ltComboBox4.BorderRadius = ((byte)(20));
+            this.ltComboBox4.BorderSize = ((byte)(3));
+            this.ltComboBox4.ColorPallet = System.Drawing.Color.Crimson;
+            this.ltComboBox4.EnableBorder = true;
+            this.ltComboBox4.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.ltComboBox4.FlatAppearance.BorderSize = 0;
+            this.ltComboBox4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ltComboBox4.ForeColor = System.Drawing.Color.White;
+            this.ltComboBox4.Items = new string[] {
+        "Name",
+        "Family",
+        "Age",
+        "Usercode",
+        "Phone",
+        "Address",
+        "Account Number 1",
+        "Account Number 2"};
+            this.ltComboBox4.ItemsBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ltComboBox4.ItemsCheckedColor = System.Drawing.Color.Crimson;
+            this.ltComboBox4.ItemsFont = new System.Drawing.Font("Segoe UI Semibold", 9F);
+            this.ltComboBox4.ItemsForeColor = System.Drawing.Color.White;
+            this.ltComboBox4.ItemsHeight = ((byte)(40));
+            this.ltComboBox4.Location = new System.Drawing.Point(394, 11);
+            this.ltComboBox4.Name = "ltComboBox4";
+            this.ltComboBox4.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.ltComboBox4.Size = new System.Drawing.Size(175, 45);
+            this.ltComboBox4.TabIndex = 1;
+            this.ltComboBox4.Text = "Search Based";
+            this.ltComboBox4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ltComboBox4.TopDirection = false;
+            this.ltComboBox4.UseVisualStyleBackColor = true;
+            // 
+            // roundedPanel3
+            // 
+            this.roundedPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.roundedPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.roundedPanel3.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedPanel3.BorderSize = ((byte)(3));
+            this.roundedPanel3.EnableBorder = true;
+            this.roundedPanel3.Location = new System.Drawing.Point(25, 145);
+            this.roundedPanel3.Name = "roundedPanel3";
+            this.roundedPanel3.Size = new System.Drawing.Size(900, 551);
+            this.roundedPanel3.TabIndex = 4;
+            // 
+            // roundedPanel5
+            // 
+            this.roundedPanel5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.roundedPanel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.roundedPanel5.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedPanel5.BorderSize = ((byte)(3));
+            this.roundedPanel5.EnableBorder = true;
+            this.roundedPanel5.Location = new System.Drawing.Point(941, 145);
+            this.roundedPanel5.Name = "roundedPanel5";
+            this.roundedPanel5.Size = new System.Drawing.Size(411, 551);
+            this.roundedPanel5.TabIndex = 4;
             // 
             // SearchUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.ClientSize = new System.Drawing.Size(1260, 690);
+            this.ClientSize = new System.Drawing.Size(1371, 714);
             this.Controls.Add(this.roundedPanel2);
+            this.Controls.Add(this.roundedPanel5);
+            this.Controls.Add(this.roundedPanel3);
             this.Controls.Add(this.roundedPanel1);
             this.Controls.Add(this.windowToolbar1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -234,5 +336,9 @@
         private Controls.RoundedButton roundedButton5;
         private Controls.RoundedPanel roundedPanel2;
         private Controls.LTComboBox ltComboBox2;
+        private Controls.LTComboBox ltComboBox4;
+        private Controls.LTComboBox ltComboBox3;
+        private Controls.RoundedPanel roundedPanel3;
+        private Controls.RoundedPanel roundedPanel5;
     }
 }
