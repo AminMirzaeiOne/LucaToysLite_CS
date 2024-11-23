b26f6c9bfdbf34cfeb15303471799e5673bd6ab0
Amin Mirzaei (امین میرزایی) <aminmirzaei.og@gmail.com>
2024-11-23 15:53:17 +0330

    - Create Unnecessary Page In CustomerInfo

---
+++ LucaToysLite/Pages/CustomerInfo/Unnecessary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LucaToysLite.Pages.CustomerInfo
+{
+    public partial class Unnecessary : UserControl
+    {
+        public Unnecessary()
+        {
+            InitializeComponent();
+        }
+    }
+}
