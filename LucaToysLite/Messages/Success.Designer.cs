16c8bf6d7333a59c299fa53b8285bed7ef8434f2
Amin Mirzaei (امین میرزایی) <aminmirzaei.og@gmail.com>
2024-11-27 18:31:37 +0330

    - Update Success MessageBox Form

--- LucaToysLite/Messages/Success.Designer.cs
+++ LucaToysLite/Messages/Success.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(750, 450);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Success";
