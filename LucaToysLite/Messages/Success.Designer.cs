acfe654d2b11dd7ce88a72e9cdbd6c429c7e15f8
Amin Mirzaei (امین میرزایی) <aminmirzaei.og@gmail.com>
2024-11-27 18:40:34 +0330

    - Update Success MessageBox Form Ui

--- LucaToysLite/Messages/Success.Designer.cs
+++ LucaToysLite/Messages/Success.Designer.cs
@@ -28,7 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.windowToolbar1 = new LucaToysLite.Controls.WindowToolbar();
+            this.roundedButton1 = new LucaToysLite.Controls.RoundedButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ltContextMenu1 = new LucaToysLite.Controls.LTContextMenu();
+            this.copyContentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyDescriptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.searchContentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchDescriptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.defaultButtonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.ltContextMenu1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // windowToolbar1
+            // 
+            this.windowToolbar1.BackColor = System.Drawing.Color.Crimson;
+            this.windowToolbar1.BorderStyleRadius = true;
+            this.windowToolbar1.CloseMessage = true;
+            this.windowToolbar1.ColorPalette = System.Drawing.Color.Crimson;
+            this.windowToolbar1.DarkTheme = true;
+            this.windowToolbar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.windowToolbar1.IconButton = true;
+            this.windowToolbar1.IconButtonImage = null;
+            this.windowToolbar1.IconButtonSybmol = "";
+            this.windowToolbar1.IconMenu = true;
+            this.windowToolbar1.Location = new System.Drawing.Point(0, 0);
+            this.windowToolbar1.MaximizeButton = true;
+            this.windowToolbar1.MinimizeButton = true;
+            this.windowToolbar1.MoveOption = true;
+            this.windowToolbar1.Name = "windowToolbar1";
+            this.windowToolbar1.Size = new System.Drawing.Size(750, 45);
+            this.windowToolbar1.StartupSound = LucaToysLite.Controls.WindowToolbar.StartupSounds.None;
+            this.windowToolbar1.TabIndex = 0;
+            this.windowToolbar1.TitleText = "Success";
+            this.windowToolbar1.WindowBorderSize = ((byte)(3));
+            // 
+            // roundedButton1
+            // 
+            this.roundedButton1.BackColor = System.Drawing.Color.Crimson;
+            this.roundedButton1.BorderRadius = ((byte)(20));
+            this.roundedButton1.BorderSize = ((byte)(3));
+            this.roundedButton1.EnableBorder = false;
+            this.roundedButton1.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedButton1.FlatAppearance.BorderSize = 0;
+            this.roundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButton1.ForeColor = System.Drawing.Color.White;
+            this.roundedButton1.Location = new System.Drawing.Point(609, 393);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.Size = new System.Drawing.Size(124, 40);
+            this.roundedButton1.TabIndex = 1;
+            this.roundedButton1.Text = "OK";
+            this.roundedButton1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::LucaToysLite.Properties.Resources.icons8_info_96;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(259, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(464, 131);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "asdfsadfklasdflas;df\r\nasdflasjdfkasdfasdflasfj\r\nasdfasdlfkajsldfjasldfjaslfd";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(259, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(464, 116);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Close This Window";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ltContextMenu1
+            // 
+            this.ltContextMenu1.Background = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ltContextMenu1.BorderSize = ((byte)(2));
+            this.ltContextMenu1.ColorPallet = System.Drawing.Color.Crimson;
+            this.ltContextMenu1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.ltContextMenu1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyContentToolStripMenuItem,
+            this.copyDescriptionToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.searchContentToolStripMenuItem,
+            this.searchDescriptionToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.defaultButtonToolStripMenuItem});
+            this.ltContextMenu1.Name = "ltContextMenu1";
+            this.ltContextMenu1.Size = new System.Drawing.Size(232, 176);
+            // 
+            // copyContentToolStripMenuItem
+            // 
+            this.copyContentToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.copyContentToolStripMenuItem.Name = "copyContentToolStripMenuItem";
+            this.copyContentToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.copyContentToolStripMenuItem.Text = "Copy Content";
+            // 
+            // copyDescriptionToolStripMenuItem
+            // 
+            this.copyDescriptionToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.copyDescriptionToolStripMenuItem.Name = "copyDescriptionToolStripMenuItem";
+            this.copyDescriptionToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.copyDescriptionToolStripMenuItem.Text = "Copy Description";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(237, 6);
+            // 
+            // searchContentToolStripMenuItem
+            // 
+            this.searchContentToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.searchContentToolStripMenuItem.Name = "searchContentToolStripMenuItem";
+            this.searchContentToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.searchContentToolStripMenuItem.Text = "Search Content";
+            // 
+            // searchDescriptionToolStripMenuItem
+            // 
+            this.searchDescriptionToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.searchDescriptionToolStripMenuItem.Name = "searchDescriptionToolStripMenuItem";
+            this.searchDescriptionToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.searchDescriptionToolStripMenuItem.Text = "Search Description";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(237, 6);
+            // 
+            // defaultButtonToolStripMenuItem
+            // 
+            this.defaultButtonToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.defaultButtonToolStripMenuItem.Name = "defaultButtonToolStripMenuItem";
+            this.defaultButtonToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.defaultButtonToolStripMenuItem.Text = "Default Button";
             // 
             // Success
             // 
@@ -36,15 +185,37 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.ClientSize = new System.Drawing.Size(750, 450);
+            this.ContextMenuStrip = this.ltContextMenu1;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.roundedButton1);
+            this.Controls.Add(this.windowToolbar1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Success";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Success";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ltContextMenu1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Controls.WindowToolbar windowToolbar1;
+        private Controls.RoundedButton roundedButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Controls.LTContextMenu ltContextMenu1;
+        private System.Windows.Forms.ToolStripMenuItem copyContentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyDescriptionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem searchContentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchDescriptionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem defaultButtonToolStripMenuItem;
     }
 }
