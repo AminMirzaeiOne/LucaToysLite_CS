6e7ee684613e6c9737e8a54ac94421698823adc1
Amin Mirzaei (امین میرزایی) <aminmirzaei.og@gmail.com>
2024-11-28 01:23:27 +0330

    - Update PhoneBook Form Ui

--- LucaToysLite/Apps/PhoneBook.Designer.cs
+++ LucaToysLite/Apps/PhoneBook.Designer.cs
@@ -28,44 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.windowToolbar1 = new LucaToysLite.Controls.WindowToolbar();
+            this.roundedPanel5 = new LucaToysLite.Controls.RoundedPanel();
+            this.roundedPanel2 = new LucaToysLite.Controls.RoundedPanel();
+            this.roundedPanel1 = new LucaToysLite.Controls.RoundedPanel();
             this.roundedPanel3 = new LucaToysLite.Controls.RoundedPanel();
-            this.roundedButton5 = new LucaToysLite.Controls.RoundedButton();
-            this.roundedPanel4 = new LucaToysLite.Controls.RoundedPanel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ltRadioButton4 = new LucaToysLite.Controls.LTRadioButton();
             this.ltRadioButton3 = new LucaToysLite.Controls.LTRadioButton();
             this.ltRadioButton2 = new LucaToysLite.Controls.LTRadioButton();
             this.ltRadioButton1 = new LucaToysLite.Controls.LTRadioButton();
-            this.ltRadioButton4 = new LucaToysLite.Controls.LTRadioButton();
-            this.roundedPanel1 = new LucaToysLite.Controls.RoundedPanel();
-            this.roundedPanel2 = new LucaToysLite.Controls.RoundedPanel();
-            this.roundedPanel5 = new LucaToysLite.Controls.RoundedPanel();
+            this.roundedButton5 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedPanel4 = new LucaToysLite.Controls.RoundedPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.windowToolbar1 = new LucaToysLite.Controls.WindowToolbar();
             this.roundedPanel3.SuspendLayout();
             this.roundedPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
-            // windowToolbar1
+            // roundedPanel5
             // 
-            this.windowToolbar1.BackColor = System.Drawing.Color.Crimson;
-            this.windowToolbar1.BorderStyleRadius = true;
-            this.windowToolbar1.CloseMessage = true;
-            this.windowToolbar1.ColorPalette = System.Drawing.Color.Crimson;
-            this.windowToolbar1.DarkTheme = true;
-            this.windowToolbar1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.windowToolbar1.IconButton = true;
-            this.windowToolbar1.IconButtonImage = null;
-            this.windowToolbar1.IconButtonSybmol = "";
-            this.windowToolbar1.IconMenu = true;
-            this.windowToolbar1.Location = new System.Drawing.Point(0, 0);
-            this.windowToolbar1.MaximizeButton = true;
-            this.windowToolbar1.MinimizeButton = true;
-            this.windowToolbar1.MoveOption = true;
-            this.windowToolbar1.Name = "windowToolbar1";
-            this.windowToolbar1.Size = new System.Drawing.Size(1106, 45);
-            this.windowToolbar1.StartupSound = LucaToysLite.Controls.WindowToolbar.StartupSounds.None;
-            this.windowToolbar1.TabIndex = 0;
-            this.windowToolbar1.TitleText = "Phone Book";
-            this.windowToolbar1.WindowBorderSize = ((byte)(3));
+            this.roundedPanel5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.roundedPanel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.roundedPanel5.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedPanel5.BorderSize = ((byte)(3));
+            this.roundedPanel5.EnableBorder = true;
+            this.roundedPanel5.Location = new System.Drawing.Point(922, 153);
+            this.roundedPanel5.Name = "roundedPanel5";
+            this.roundedPanel5.Size = new System.Drawing.Size(162, 517);
+            this.roundedPanel5.TabIndex = 13;
+            // 
+            // roundedPanel2
+            // 
+            this.roundedPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.roundedPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.roundedPanel2.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedPanel2.BorderSize = ((byte)(3));
+            this.roundedPanel2.EnableBorder = true;
+            this.roundedPanel2.Location = new System.Drawing.Point(328, 153);
+            this.roundedPanel2.Name = "roundedPanel2";
+            this.roundedPanel2.Size = new System.Drawing.Size(575, 517);
+            this.roundedPanel2.TabIndex = 13;
+            // 
+            // roundedPanel1
+            // 
+            this.roundedPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.roundedPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.roundedPanel1.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedPanel1.BorderSize = ((byte)(3));
+            this.roundedPanel1.EnableBorder = true;
+            this.roundedPanel1.Location = new System.Drawing.Point(19, 153);
+            this.roundedPanel1.Name = "roundedPanel1";
+            this.roundedPanel1.Size = new System.Drawing.Size(290, 517);
+            this.roundedPanel1.TabIndex = 13;
             // 
             // roundedPanel3
             // 
@@ -86,50 +103,21 @@
             this.roundedPanel3.Size = new System.Drawing.Size(1065, 80);
             this.roundedPanel3.TabIndex = 13;
             // 
-            // roundedButton5
+            // ltRadioButton4
             // 
-            this.roundedButton5.BorderRadius = ((byte)(20));
-            this.roundedButton5.BorderSize = ((byte)(3));
-            this.roundedButton5.EnableBorder = true;
-            this.roundedButton5.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
-            this.roundedButton5.FlatAppearance.BorderSize = 0;
-            this.roundedButton5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
-            this.roundedButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.roundedButton5.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.roundedButton5.ForeColor = System.Drawing.Color.White;
-            this.roundedButton5.Image = global::LucaToysLite.Properties.Resources.icons8_search_32;
-            this.roundedButton5.Location = new System.Drawing.Point(334, 17);
-            this.roundedButton5.Name = "roundedButton5";
-            this.roundedButton5.Size = new System.Drawing.Size(130, 45);
-            this.roundedButton5.TabIndex = 2;
-            this.roundedButton5.Text = " Search";
-            this.roundedButton5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.roundedButton5.UseVisualStyleBackColor = true;
-            // 
-            // roundedPanel4
-            // 
-            this.roundedPanel4.BorderColor = System.Drawing.Color.Crimson;
-            this.roundedPanel4.BorderSize = ((byte)(3));
-            this.roundedPanel4.Controls.Add(this.textBox1);
-            this.roundedPanel4.EnableBorder = true;
-            this.roundedPanel4.Location = new System.Drawing.Point(20, 17);
-            this.roundedPanel4.Name = "roundedPanel4";
-            this.roundedPanel4.Size = new System.Drawing.Size(299, 45);
-            this.roundedPanel4.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.ForeColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(14, 9);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(269, 26);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Search Contact";
+            this.ltRadioButton4.AutoSize = true;
+            this.ltRadioButton4.CheckedColor = System.Drawing.Color.Crimson;
+            this.ltRadioButton4.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltRadioButton4.ForeColor = System.Drawing.Color.White;
+            this.ltRadioButton4.Location = new System.Drawing.Point(835, 26);
+            this.ltRadioButton4.MinimumSize = new System.Drawing.Size(0, 21);
+            this.ltRadioButton4.Name = "ltRadioButton4";
+            this.ltRadioButton4.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.ltRadioButton4.Size = new System.Drawing.Size(133, 29);
+            this.ltRadioButton4.TabIndex = 10;
+            this.ltRadioButton4.Text = "Telephone";
+            this.ltRadioButton4.UnCheckedColor = System.Drawing.Color.Gray;
+            this.ltRadioButton4.UseVisualStyleBackColor = true;
             // 
             // ltRadioButton3
             // 
@@ -181,60 +169,73 @@
             this.ltRadioButton1.UnCheckedColor = System.Drawing.Color.Gray;
             this.ltRadioButton1.UseVisualStyleBackColor = true;
             // 
-            // ltRadioButton4
+            // roundedButton5
             // 
-            this.ltRadioButton4.AutoSize = true;
-            this.ltRadioButton4.CheckedColor = System.Drawing.Color.Crimson;
-            this.ltRadioButton4.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ltRadioButton4.ForeColor = System.Drawing.Color.White;
-            this.ltRadioButton4.Location = new System.Drawing.Point(835, 26);
-            this.ltRadioButton4.MinimumSize = new System.Drawing.Size(0, 21);
-            this.ltRadioButton4.Name = "ltRadioButton4";
-            this.ltRadioButton4.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.ltRadioButton4.Size = new System.Drawing.Size(133, 29);
-            this.ltRadioButton4.TabIndex = 10;
-            this.ltRadioButton4.Text = "Telephone";
-            this.ltRadioButton4.UnCheckedColor = System.Drawing.Color.Gray;
-            this.ltRadioButton4.UseVisualStyleBackColor = true;
+            this.roundedButton5.BorderRadius = ((byte)(20));
+            this.roundedButton5.BorderSize = ((byte)(3));
+            this.roundedButton5.EnableBorder = true;
+            this.roundedButton5.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedButton5.FlatAppearance.BorderSize = 0;
+            this.roundedButton5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.roundedButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton5.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButton5.ForeColor = System.Drawing.Color.White;
+            this.roundedButton5.Image = global::LucaToysLite.Properties.Resources.icons8_search_32;
+            this.roundedButton5.Location = new System.Drawing.Point(334, 17);
+            this.roundedButton5.Name = "roundedButton5";
+            this.roundedButton5.Size = new System.Drawing.Size(130, 45);
+            this.roundedButton5.TabIndex = 2;
+            this.roundedButton5.Text = " Search";
+            this.roundedButton5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.roundedButton5.UseVisualStyleBackColor = true;
             // 
-            // roundedPanel1
+            // roundedPanel4
             // 
-            this.roundedPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.roundedPanel4.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedPanel4.BorderSize = ((byte)(3));
+            this.roundedPanel4.Controls.Add(this.textBox1);
+            this.roundedPanel4.EnableBorder = true;
+            this.roundedPanel4.Location = new System.Drawing.Point(20, 17);
+            this.roundedPanel4.Name = "roundedPanel4";
+            this.roundedPanel4.Size = new System.Drawing.Size(299, 45);
+            this.roundedPanel4.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.roundedPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
-            this.roundedPanel1.BorderColor = System.Drawing.Color.Crimson;
-            this.roundedPanel1.BorderSize = ((byte)(3));
-            this.roundedPanel1.EnableBorder = true;
-            this.roundedPanel1.Location = new System.Drawing.Point(19, 153);
-            this.roundedPanel1.Name = "roundedPanel1";
-            this.roundedPanel1.Size = new System.Drawing.Size(319, 517);
-            this.roundedPanel1.TabIndex = 13;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(14, 9);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Search Contact";
             // 
-            // roundedPanel2
+            // windowToolbar1
             // 
-            this.roundedPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.roundedPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
-            this.roundedPanel2.BorderColor = System.Drawing.Color.Crimson;
-            this.roundedPanel2.BorderSize = ((byte)(3));
-            this.roundedPanel2.EnableBorder = true;
-            this.roundedPanel2.Location = new System.Drawing.Point(354, 153);
-            this.roundedPanel2.Name = "roundedPanel2";
-            this.roundedPanel2.Size = new System.Drawing.Size(550, 517);
-            this.roundedPanel2.TabIndex = 13;
-            // 
-            // roundedPanel5
-            // 
-            this.roundedPanel5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.roundedPanel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
-            this.roundedPanel5.BorderColor = System.Drawing.Color.Crimson;
-            this.roundedPanel5.BorderSize = ((byte)(3));
-            this.roundedPanel5.EnableBorder = true;
-            this.roundedPanel5.Location = new System.Drawing.Point(922, 153);
-            this.roundedPanel5.Name = "roundedPanel5";
-            this.roundedPanel5.Size = new System.Drawing.Size(162, 517);
-            this.roundedPanel5.TabIndex = 13;
+            this.windowToolbar1.BackColor = System.Drawing.Color.Crimson;
+            this.windowToolbar1.BorderStyleRadius = true;
+            this.windowToolbar1.CloseMessage = true;
+            this.windowToolbar1.ColorPalette = System.Drawing.Color.Crimson;
+            this.windowToolbar1.DarkTheme = true;
+            this.windowToolbar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.windowToolbar1.IconButton = true;
+            this.windowToolbar1.IconButtonImage = null;
+            this.windowToolbar1.IconButtonSybmol = "";
+            this.windowToolbar1.IconMenu = true;
+            this.windowToolbar1.Location = new System.Drawing.Point(0, 0);
+            this.windowToolbar1.MaximizeButton = true;
+            this.windowToolbar1.MinimizeButton = true;
+            this.windowToolbar1.MoveOption = true;
+            this.windowToolbar1.Name = "windowToolbar1";
+            this.windowToolbar1.Size = new System.Drawing.Size(1106, 45);
+            this.windowToolbar1.StartupSound = LucaToysLite.Controls.WindowToolbar.StartupSounds.None;
+            this.windowToolbar1.TabIndex = 0;
+            this.windowToolbar1.TitleText = "Phone Book";
+            this.windowToolbar1.WindowBorderSize = ((byte)(3));
             // 
             // PhoneBook
             // 
