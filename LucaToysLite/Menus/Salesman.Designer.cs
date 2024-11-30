32018e12ebcf6ce898d4e5add15cd2c41bce2f3e
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-12-01 02:15:23 +0330

    - Create Salesman Submenu In Menus Folder

---
+++ LucaToysLite/Menus/Salesman.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace LucaToysLite.Menus
+{
+    partial class Salesman
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Salesman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.Name = "Salesman";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
