4bde0bfb91b3d0865d98b5abb40609ecc6a13e45
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-12-01 22:47:43 +0330

    - Change Update MessageBox Form Code

--- LucaToysLite/Messages/Update.cs
+++ LucaToysLite/Messages/Update.cs
@@ -15,6 +15,89 @@ namespace LucaToysLite.Messages
         public Update()
         {
             InitializeComponent();
+            this.FocusEnable(this.Owner);
+        }
+
+        private void Success_Load(object sender, EventArgs e)
+        {
+            this.loadingControl1.StartProgress();
+        }
+
+        private void roundedButton1_Click(object sender, EventArgs e)
+        {
+            this.windowToolbar1.Close();
+            this.ActiveOwner();
+        }
+
+        private Form frm = new Form();
+        public void FocusEnable(Form window)
+        {
+
+            this.frm.FormBorderStyle = FormBorderStyle.None;
+            this.frm.BackColor = Color.Black;
+            this.frm.Opacity = 0.6;
+            this.frm.Show();
+            this.frm.Location = window.Location;
+            this.frm.Size = window.Size;
+
+        }
+
+        private void ActiveOwner()
+        {
+            FormWindowState windowState = FormWindowState.Normal;
+            windowState = this.Owner.WindowState;
+            this.Owner.WindowState = FormWindowState.Minimized;
+            this.Owner.WindowState = windowState;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            this.frm.Close();
+        }
+
+        public Update(Form owner, string title, string content)
+        {
+            InitializeComponent();
+            this.Owner = owner;
+            this.TitleText = title;
+            this.ContentText = content;
+            this.windowToolbar1.CloseMessage = false;
+            this.FocusEnable(this.Owner);
+        }
+
+        public Update(Form owner, string title, string content, string description)
+        {
+            InitializeComponent();
+            this.Owner = owner;
+            this.TitleText = title;
+            this.ContentText = content;
+            this.DescriptionText = description;
+            this.windowToolbar1.CloseMessage = false;
+            this.FocusEnable(this.Owner);
+
+        }
+
+
+
+
+
+        public System.String TitleText
+        {
+            get { return this.windowToolbar1.TitleText; }
+            set { this.windowToolbar1.TitleText = value; }
+        }
+
+        public System.String ContentText
+        {
+            get { return this.label2.Text; }
+            set { this.label2.Text = value; }
+        }
+
+        public System.String DescriptionText
+        {
+            get { return this.label1.Text; }
+            set { this.label1.Text = value; }
         }
     }
 }
