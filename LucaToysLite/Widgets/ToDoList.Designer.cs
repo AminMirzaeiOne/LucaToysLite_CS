1905b944c8694c482075a566850875679c3cf45e
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-12-02 01:15:31 +0330

    - Update ToDoList Widget Ui

--- LucaToysLite/Widgets/ToDoList.Designer.cs
+++ LucaToysLite/Widgets/ToDoList.Designer.cs
@@ -28,20 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.roundedLabel1 = new LucaToysLite.Controls.RoundedLabel();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 13F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Manuscripts";
+            // 
+            // roundedLabel1
+            // 
+            this.roundedLabel1.BackColor = System.Drawing.Color.Crimson;
+            this.roundedLabel1.BorderRadius = ((byte)(4));
+            this.roundedLabel1.Location = new System.Drawing.Point(3, 78);
+            this.roundedLabel1.Name = "roundedLabel1";
+            this.roundedLabel1.Size = new System.Drawing.Size(357, 4);
+            this.roundedLabel1.TabIndex = 25;
             // 
             // ToDoList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.Controls.Add(this.roundedLabel1);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "ToDoList";
-            this.Size = new System.Drawing.Size(234, 284);
+            this.Size = new System.Drawing.Size(360, 630);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Controls.RoundedLabel roundedLabel1;
     }
 }
