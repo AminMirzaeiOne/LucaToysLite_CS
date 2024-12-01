7fad7c10d43e19f681ca981adcd36bdd32ce8216
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-12-02 00:09:10 +0330

    - Update AddBrand Form Ui

--- LucaToysLite/Forms/AddBrand.Designer.cs
+++ LucaToysLite/Forms/AddBrand.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.windowToolbar1 = new LucaToysLite.Controls.WindowToolbar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.roundedButton2 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedButton1 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedPanel2 = new LucaToysLite.Controls.RoundedPanel();
+            this.roundedPanel1 = new LucaToysLite.Controls.RoundedPanel();
             this.SuspendLayout();
             // 
             // windowToolbar1
@@ -48,19 +53,94 @@
             this.windowToolbar1.MinimizeButton = true;
             this.windowToolbar1.MoveOption = true;
             this.windowToolbar1.Name = "windowToolbar1";
-            this.windowToolbar1.Size = new System.Drawing.Size(845, 45);
+            this.windowToolbar1.Size = new System.Drawing.Size(1025, 45);
             this.windowToolbar1.StartupSound = LucaToysLite.Controls.WindowToolbar.StartupSounds.None;
             this.windowToolbar1.TabIndex = 0;
             this.windowToolbar1.TitleText = "Add Brand";
             this.windowToolbar1.WindowBorderSize = ((byte)(3));
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.BackColor = System.Drawing.Color.Crimson;
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(223, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(3, 548);
+            this.label1.TabIndex = 15;
+            // 
+            // roundedButton2
+            // 
+            this.roundedButton2.BorderRadius = ((byte)(20));
+            this.roundedButton2.BorderSize = ((byte)(3));
+            this.roundedButton2.EnableBorder = true;
+            this.roundedButton2.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedButton2.FlatAppearance.BorderSize = 0;
+            this.roundedButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.roundedButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton2.ForeColor = System.Drawing.Color.White;
+            this.roundedButton2.Location = new System.Drawing.Point(18, 529);
+            this.roundedButton2.Name = "roundedButton2";
+            this.roundedButton2.Size = new System.Drawing.Size(185, 50);
+            this.roundedButton2.TabIndex = 13;
+            this.roundedButton2.Text = "Change Photo";
+            this.roundedButton2.UseVisualStyleBackColor = true;
+            // 
+            // roundedButton1
+            // 
+            this.roundedButton1.BorderRadius = ((byte)(20));
+            this.roundedButton1.BorderSize = ((byte)(3));
+            this.roundedButton1.EnableBorder = true;
+            this.roundedButton1.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedButton1.FlatAppearance.BorderSize = 0;
+            this.roundedButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.roundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton1.ForeColor = System.Drawing.Color.White;
+            this.roundedButton1.Location = new System.Drawing.Point(18, 259);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.Size = new System.Drawing.Size(185, 50);
+            this.roundedButton1.TabIndex = 14;
+            this.roundedButton1.Text = "Change Photo";
+            this.roundedButton1.UseVisualStyleBackColor = true;
+            // 
+            // roundedPanel2
+            // 
+            this.roundedPanel2.BackgroundImage = global::LucaToysLite.Properties.Resources.Group_33;
+            this.roundedPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.roundedPanel2.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedPanel2.BorderSize = ((byte)(3));
+            this.roundedPanel2.EnableBorder = true;
+            this.roundedPanel2.Location = new System.Drawing.Point(18, 335);
+            this.roundedPanel2.Name = "roundedPanel2";
+            this.roundedPanel2.Size = new System.Drawing.Size(185, 180);
+            this.roundedPanel2.TabIndex = 11;
+            // 
+            // roundedPanel1
+            // 
+            this.roundedPanel1.BackgroundImage = global::LucaToysLite.Properties.Resources.Group_34;
+            this.roundedPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.roundedPanel1.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedPanel1.BorderSize = ((byte)(3));
+            this.roundedPanel1.EnableBorder = true;
+            this.roundedPanel1.Location = new System.Drawing.Point(18, 67);
+            this.roundedPanel1.Name = "roundedPanel1";
+            this.roundedPanel1.Size = new System.Drawing.Size(185, 180);
+            this.roundedPanel1.TabIndex = 12;
             // 
             // AddBrand
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.ClientSize = new System.Drawing.Size(845, 525);
+            this.ClientSize = new System.Drawing.Size(1025, 600);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.roundedButton2);
+            this.Controls.Add(this.roundedButton1);
+            this.Controls.Add(this.roundedPanel2);
+            this.Controls.Add(this.roundedPanel1);
             this.Controls.Add(this.windowToolbar1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AddBrand";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -72,5 +152,10 @@
         #endregion
 
         private Controls.WindowToolbar windowToolbar1;
+        private System.Windows.Forms.Label label1;
+        private Controls.RoundedButton roundedButton2;
+        private Controls.RoundedButton roundedButton1;
+        private Controls.RoundedPanel roundedPanel2;
+        private Controls.RoundedPanel roundedPanel1;
     }
 }
--- LucaToysLite/Properties/Resources.Designer.cs
+++ LucaToysLite/Properties/Resources.Designer.cs
@@ -423,6 +423,26 @@ namespace LucaToysLite.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Group_33 {
+            get {
+                object obj = ResourceManager.GetObject("Group 33", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Group_34 {
+            get {
+                object obj = ResourceManager.GetObject("Group 34", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Group_4 {
             get {
                 object obj = ResourceManager.GetObject("Group 4", resourceCulture);
