38a676b74fe1c83a0f5e52266b315ffab8e7622f
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-12-02 00:18:11 +0330

    - Update AddPerson Form Ui

--- LucaToysLite/Properties/Resources.Designer.cs
+++ LucaToysLite/Properties/Resources.Designer.cs
@@ -443,6 +443,26 @@ namespace LucaToysLite.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Group_35 {
+            get {
+                object obj = ResourceManager.GetObject("Group 35", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Group_36 {
+            get {
+                object obj = ResourceManager.GetObject("Group 36", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Group_4 {
             get {
                 object obj = ResourceManager.GetObject("Group 4", resourceCulture);
