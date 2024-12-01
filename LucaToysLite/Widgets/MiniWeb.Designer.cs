9e54cbf278331f199ff8476bc40975f8bd0c82e2
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-12-02 01:05:54 +0330

    - Update MiniWeb Widget Ui

--- LucaToysLite/Widgets/MiniWeb.Designer.cs
+++ LucaToysLite/Widgets/MiniWeb.Designer.cs
@@ -28,20 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.roundedPanel6 = new LucaToysLite.Controls.RoundedPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.roundedButton10 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedButton9 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedButton8 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedButton7 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedButton1 = new LucaToysLite.Controls.RoundedButton();
+            this.roundedPanel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // roundedPanel6
+            // 
+            this.roundedPanel6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.roundedPanel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.roundedPanel6.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedPanel6.BorderSize = ((byte)(3));
+            this.roundedPanel6.Controls.Add(this.label3);
+            this.roundedPanel6.Controls.Add(this.pictureBox2);
+            this.roundedPanel6.Controls.Add(this.roundedButton1);
+            this.roundedPanel6.EnableBorder = true;
+            this.roundedPanel6.Location = new System.Drawing.Point(15, 19);
+            this.roundedPanel6.Name = "roundedPanel6";
+            this.roundedPanel6.Size = new System.Drawing.Size(470, 80);
+            this.roundedPanel6.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(91, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 32);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Digikala";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::LucaToysLite.Properties.Resources.icons8_google_68;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox2.TabIndex = 23;
+            this.pictureBox2.TabStop = false;
+            // 
+            // roundedButton10
+            // 
+            this.roundedButton10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.roundedButton10.BackgroundImage = global::LucaToysLite.Properties.Resources.icons8_whatsapp_72;
+            this.roundedButton10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.roundedButton10.BorderRadius = ((byte)(20));
+            this.roundedButton10.BorderSize = ((byte)(3));
+            this.roundedButton10.EnableBorder = true;
+            this.roundedButton10.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedButton10.FlatAppearance.BorderSize = 0;
+            this.roundedButton10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.roundedButton10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton10.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButton10.ForeColor = System.Drawing.Color.White;
+            this.roundedButton10.Location = new System.Drawing.Point(385, 121);
+            this.roundedButton10.Name = "roundedButton10";
+            this.roundedButton10.Size = new System.Drawing.Size(100, 100);
+            this.roundedButton10.TabIndex = 25;
+            this.roundedButton10.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.roundedButton10.UseVisualStyleBackColor = true;
+            // 
+            // roundedButton9
+            // 
+            this.roundedButton9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.roundedButton9.BackgroundImage = global::LucaToysLite.Properties.Resources.icons8_twitter_circled_72;
+            this.roundedButton9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.roundedButton9.BorderRadius = ((byte)(20));
+            this.roundedButton9.BorderSize = ((byte)(3));
+            this.roundedButton9.EnableBorder = true;
+            this.roundedButton9.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedButton9.FlatAppearance.BorderSize = 0;
+            this.roundedButton9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.roundedButton9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton9.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButton9.ForeColor = System.Drawing.Color.White;
+            this.roundedButton9.Location = new System.Drawing.Point(259, 121);
+            this.roundedButton9.Name = "roundedButton9";
+            this.roundedButton9.Size = new System.Drawing.Size(100, 100);
+            this.roundedButton9.TabIndex = 26;
+            this.roundedButton9.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.roundedButton9.UseVisualStyleBackColor = true;
+            // 
+            // roundedButton8
+            // 
+            this.roundedButton8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.roundedButton8.BackgroundImage = global::LucaToysLite.Properties.Resources.icons8_instagram_72;
+            this.roundedButton8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.roundedButton8.BorderRadius = ((byte)(20));
+            this.roundedButton8.BorderSize = ((byte)(3));
+            this.roundedButton8.EnableBorder = true;
+            this.roundedButton8.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedButton8.FlatAppearance.BorderSize = 0;
+            this.roundedButton8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.roundedButton8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton8.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButton8.ForeColor = System.Drawing.Color.White;
+            this.roundedButton8.Location = new System.Drawing.Point(135, 121);
+            this.roundedButton8.Name = "roundedButton8";
+            this.roundedButton8.Size = new System.Drawing.Size(100, 100);
+            this.roundedButton8.TabIndex = 27;
+            this.roundedButton8.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.roundedButton8.UseVisualStyleBackColor = true;
+            // 
+            // roundedButton7
+            // 
+            this.roundedButton7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.roundedButton7.BackgroundImage = global::LucaToysLite.Properties.Resources.icons8_youtube_music_72;
+            this.roundedButton7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.roundedButton7.BorderRadius = ((byte)(20));
+            this.roundedButton7.BorderSize = ((byte)(3));
+            this.roundedButton7.EnableBorder = true;
+            this.roundedButton7.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedButton7.FlatAppearance.BorderSize = 0;
+            this.roundedButton7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.roundedButton7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton7.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButton7.ForeColor = System.Drawing.Color.White;
+            this.roundedButton7.Location = new System.Drawing.Point(15, 121);
+            this.roundedButton7.Name = "roundedButton7";
+            this.roundedButton7.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.roundedButton7.Size = new System.Drawing.Size(100, 100);
+            this.roundedButton7.TabIndex = 28;
+            this.roundedButton7.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.roundedButton7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.roundedButton7.UseVisualStyleBackColor = true;
+            // 
+            // roundedButton1
+            // 
+            this.roundedButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.roundedButton1.BackColor = System.Drawing.Color.Crimson;
+            this.roundedButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.roundedButton1.BorderRadius = ((byte)(20));
+            this.roundedButton1.BorderSize = ((byte)(3));
+            this.roundedButton1.EnableBorder = false;
+            this.roundedButton1.FlatAppearance.BorderColor = System.Drawing.Color.Crimson;
+            this.roundedButton1.FlatAppearance.BorderSize = 0;
+            this.roundedButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.roundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButton1.ForeColor = System.Drawing.Color.White;
+            this.roundedButton1.Location = new System.Drawing.Point(410, 19);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.Size = new System.Drawing.Size(45, 45);
+            this.roundedButton1.TabIndex = 27;
+            this.roundedButton1.Text = "";
+            this.roundedButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.roundedButton1.UseVisualStyleBackColor = false;
             // 
             // MiniWeb
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.Controls.Add(this.roundedButton10);
+            this.Controls.Add(this.roundedButton9);
+            this.Controls.Add(this.roundedButton8);
+            this.Controls.Add(this.roundedButton7);
+            this.Controls.Add(this.roundedPanel6);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "MiniWeb";
-            this.Size = new System.Drawing.Size(450, 550);
+            this.Size = new System.Drawing.Size(500, 700);
+            this.roundedPanel6.ResumeLayout(false);
+            this.roundedPanel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Controls.RoundedPanel roundedPanel6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Controls.RoundedButton roundedButton10;
+        private Controls.RoundedButton roundedButton9;
+        private Controls.RoundedButton roundedButton8;
+        private Controls.RoundedButton roundedButton7;
+        private Controls.RoundedButton roundedButton1;
     }
 }
